621de2ec9989e631370802c5170dc36689ed59d9
Weslley Campos <weslley_campos@outlook.com>
2018-07-31 02:27:38 -0300

    Created UserManager class as well as  listing and create method.

---
+++ Business/Business/UserManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Persistence.Persistence;
+using Model.Models;
+
+namespace Business.Business
+{
+    public class UserManager
+    {
+        UserPersistence userPersistence;
+
+        public UserManager() => userPersistence = new UserPersistence();
+
+        public List<UserDefault> GetAll() => userPersistence.GetAll();
+
+        public void Create(UserDefault user) => userPersistence.Add(user);
+    }
+}
