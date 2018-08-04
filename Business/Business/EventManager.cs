ee85680cc3f9272fb039a1b62f648a8dbe4a3500
Thiago <thi.alessandro@hotmail.com>
2018-08-03 21:45:51 -0300

    Criada gerenciadora do Evento

---
+++ Business/Business/EventManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Persistence.Persistence;
+using Model.Models;
+
+namespace Business.Business
+{
+    public class EventManager
+    {
+        EventPersistence eventPersistence;
+
+        public EventManager() => eventPersistence = new EventPersistence();
+
+        public List<Event> GetAll() => eventPersistence.GetAll();
+
+        public Event GetBy(int id) => eventPersistence.GetBy(id);
+
+        public void Create(Event @event) => eventPersistence.Add(@event);
+
+        public void Edit(Event @event) => eventPersistence.Update(@event);
+
+        public void Delete(int id) => eventPersistence.Delete(id);
+    }
+}
