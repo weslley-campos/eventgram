230764cb5a75a5d7fec0b0b32900c83d8150cdc8
Thiago <thi.alessandro@hotmail.com>
2018-08-03 21:47:25 -0300

    Criada controladora de Evento

---
+++ Web/Controllers/EventController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Business.Business;
+using Model.Models;
+
+namespace Web.Controllers
+{
+    public class EventController : Controller
+    {
+        EventManager eventManager;
+
+        public EventController() => eventManager = new EventManager();
+
+        public IActionResult Index() => View(eventManager.GetAll());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                eventManager.Create(GetEventData(0, collection));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public Event GetEventData(int id, IFormCollection collection)
+        {
+            Event myEvent = new Event();
+            myEvent.Id = id;
+            myEvent.EventName = collection["Nome"];
+            myEvent.EventDescription = collection["description"];
+            myEvent.EventLocation = collection["location"];
+            myEvent.EventDate = collection["eventDate"];
+            myEvent.EventModerator = collection["moderator"];
+            return myEvent;
+        }
+
+        public IActionResult Edit(int id) => View(eventManager.GetBy(id));
+
+        [HttpPost]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                eventManager.Edit(GetEventData(id, collection));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Delete(int id) => View(eventManager.GetBy(id));
+
+        [HttpPost]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                eventManager.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
