aa90f3c3cd985c97e8d524adf5deff5ae3e0a3a8
Weslley Campos <weslley_campos@outlook.com>
2018-08-10 00:59:55 -0300

    Created validation for forms field in user default class.

--- Model/Models/UserDefault.cs
+++ Model/Models/UserDefault.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Model.Models
@@ -7,6 +8,7 @@ namespace Model.Models
     public class UserDefault
     {
         private int id;
+        
         private string nome;
         private string email;
         private string senha;
@@ -19,10 +21,20 @@ namespace Model.Models
             Email = email;
             Senha = senha;
         }
-
+        [Key]
         public int Id { get => id; set => id = value; }
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "Números e caracteres especiais não são permitidos no nome.")]  
         public string Nome { get => nome; set => nome = value; }
+
+        [Required(ErrorMessage = "O e-mail do usuário é obrigatório")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get => email; set => email = value; }
+
+        [Required(ErrorMessage = "A senha do usuário é obrigatória")]
+        [StringLength(100, MinimumLength = 8)]
+        [DataType(DataType.Password)]
         public string Senha { get => senha; set => senha = value; }
     }
 }
