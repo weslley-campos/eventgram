4570cedd8764842c4a2fd393942188cbd984f7eb
Weslley Campos <weslley_campos@outlook.com>
2018-08-20 19:49:04 -0300

    Created controller Login Manager

---
+++ Business/Business/LoginManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.Models;
+using Persistence.Persistence;
+
+namespace Business.Business
+{
+
+    public class LoginManager
+    {
+        UserPersistence userPersistence = new UserPersistence();
+
+        public int? Validate(string email, string senha) => userPersistence.Validate(email, senha);
+       
+    }
+}
