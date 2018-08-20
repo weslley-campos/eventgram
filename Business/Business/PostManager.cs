354878900924557a58e2440b293dbecb3e5bb8a3
Thiago <thi.alessandro@hotmail.com>
2018-08-20 07:47:42 -0300

    Criada gerenciadora para Postagem

---
+++ Business/Business/PostManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Persistence.Persistence;
+using Model.Models;
+
+namespace Business.Business
+{
+    public class PostManager
+    {
+        PostPersistence postPersistence;
+
+        public PostManager() => postPersistence = new PostPersistence();
+
+        public List<Post> GetAll() => postPersistence.GetAll();
+
+        public Post GetBy(int id) => postPersistence.GetBy(id);
+
+        public void Create(Post post) => postPersistence.Add(post);
+
+        public void Edit(Post post) => postPersistence.Update(post);
+
+        public void Delete(int id) => postPersistence.Delete(id);
+    }
+}
